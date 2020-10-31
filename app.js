5592d98f05f995c3a2f7f88e878765e58f71ab1e
Woo-yeong Kim <kimwy1997@gmail.com>
2020-11-01 06:41:05 +0900

    Add user route to app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/users");
 
 mongoose.connect(
   `mongodb+srv://admin:${process.env.MONGO_ATLAS_PW}@node-rest-shop.pyssi.mongodb.net/${process.env.MONGO_ATLAS_DBNAME}?retryWrites=true&w=majority`,
@@ -35,6 +36,7 @@ app.use((req, res, next) => {
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/users", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
