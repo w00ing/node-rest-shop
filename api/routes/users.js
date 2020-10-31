8de0f9beeb100dc5b44fc1c7c771ad1ca658879c
Woo-yeong Kim <kimwy1997@gmail.com>
2020-11-01 06:54:19 +0900

    Create request to create user

---
+++ api/routes/users.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user");
+
+router.post("/signup", async (req, res, next) => {
+  try {
+    const user = await User.find({ email: req.body.email }).exec();
+    if (user.length >= 1) {
+      return res.status(409).json({ message: "Mail exists" });
+    } else {
+      const hash = await bcrypt.hash(req.body.password, 10);
+      const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        email: req.body.email,
+        password: hash,
+      });
+      const result = await user.save();
+      console.log(result);
+      res.status(201).json({
+        message: "User created",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error });
+  }
+});
+
+module.exports = router;
