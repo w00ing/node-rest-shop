639c146ce482274cae2914340358dba12274505f
Woo-yeong Kim <kimwy1997@gmail.com>
2020-11-01 20:35:03 +0900

    Remove unnecessary import

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 const multer = require("multer");
 const checkAuth = require("../middleware/check-auth");
 const ProductsController = require("../controllers/products");
