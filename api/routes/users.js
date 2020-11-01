6c557aea8a49beec9da0959b8429b62a4abdf2a1
Woo-yeong Kim <kimwy1997@gmail.com>
2020-11-01 16:08:58 +0900

    Create login request

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -26,6 +27,46 @@ router.post("/signup", async (req, res, next) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ error });
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.find({ email: req.body.email }).exec();
+    if (user.length < 1) {
+      return res.status(401).json({
+        message: "Auth failed",
+      });
+    }
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user[0].password,
+    );
+    if (!isPasswordCorrect) {
+      return res.status(401).json({
+        message: "Auth failed",
+      });
+    } else {
+      const token = await jwt.sign(
+        {
+          email: user[0].email,
+          userId: user[0]._id,
+        },
+        process.env.JWT_KEY,
+        {
+          expiresIn: "1h",
+        },
+      );
+      return res.status(200).json({
+        message: "Auth successful",
+        token,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(401).json({
+      message: "Auth failed",
+    });
   }
 });
 
